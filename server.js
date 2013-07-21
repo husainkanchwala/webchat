383d6221a641a50e42b738aa837fc47f6a8f7439
laukik <laukiknirgudkar@gmail.com>
2013-07-21 16:48:57 +0530

    server

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+app = express(),
+server = require('http').createServer(app),
+io = require('socket.io').listen(server),
+users = {};
+server.listen(8080);
+app.get('/',function(req,res){
+	res.sendfile( __dirname + '/index.html');
+});
+io.sockets.on('connection',function(socket){
+	socket.on('adduser',function(name){
+		socket.users = name;
+		console.log(socket.users);
+		socket.emit('reply','welcome ',name+'<br/>');
+		socket.broadcast.emit('reply',name,' is connected'+'<br/>');
+	});
+	socket.on('disconnect',function(){
+		
+	});
+});
