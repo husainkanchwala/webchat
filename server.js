60a880d413bb40531dce9740d2527c63c4f52ab5
laukik <laukiknirgudkar@gmail.com>
2013-07-27 16:16:53 +0530

    better interface to be done

--- server.js
+++ server.js
@@ -8,8 +8,13 @@ server.listen(8080);
 app.use(express.bodyParser());
 app.use(app.router);
 app.use(express.static(__dirname + '/'));
+app.use(express.cookieParser());
 app.get('/',function(req,res){
 	res.sendfile( __dirname + '/sign.html');
+});
+app.get('/index.html',function(req,res){
+	res.cookie('user',req.query.username,{httpOnly:false});
+	res.sendfile(__dirname+'/index.html');
 });
 io.sockets.on('connection',function(socket){
 	socket.on('validate',function(user,pass){
@@ -37,22 +42,21 @@ io.sockets.on('connection',function(socket){
 			if(status){
 				socket.emit('tell');
 			}
-		})
+		});
 	});
 	socket.on('newuser',function(name,pass){
 		redis.hset('users',name,pass,function(err,status){
 			if(err) throw err;
 			socket.emit('forward');
-		})
+		});
 	});
 	socket.on('adduser',function(name){
-		socket.users = name;
-		console.log(socket.users);
-		socket.emit('reply','welcome ',name+'<br/>');
+		console.log(name);
+		socket.emit('reply','welcome ',name +'<br/>');
 		socket.broadcast.emit('reply',name,' is connected'+'<br/>');
 	});
-	socket.on('msg',function(data){
-		socket.broadcast.emit('print',socket.users+' : '+data);
+	socket.on('msg',function(name,data){
+		socket.broadcast.emit('print', name + ' : '+data);
 	});
 	socket.on('disconnect',function(){
 		
